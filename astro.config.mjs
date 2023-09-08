c9b67880b8ad59ee1a9a2c1c93ad6447bed04112
Adam Poczatek <adam.p@stockopedia.com>
2023-09-08 14:58:53 +0100

    feat: home wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adampoczatek.github.io',
-  base: '/skala-studio',
+  scopedStyleStrategy: 'where'
 });
