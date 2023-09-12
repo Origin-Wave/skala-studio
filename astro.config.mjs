450a2dad83b89f5e97926b1e42441599333da4fb
Adam Poczatek <adam.p@stockopedia.com>
2023-09-12 19:52:19 +0100

    chore: clean up code base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,20 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://adampoczatek.github.io',
-  scopedStyleStrategy: 'where'
+  site: "https://www.skala-studio.co.uk",
+  scopedStyleStrategy: "where",
+  vite: {
+    resolve: {
+      alias: {
+        "@styles/": `${path.resolve(__dirname, "src")}/assets/styles/`,
+      },
+    },
+  },
 });
